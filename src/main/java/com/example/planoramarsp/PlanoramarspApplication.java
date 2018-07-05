e034f11c8a4c11ee5ffdf97dfba90884eeb786f4
Gergely Papi <gergely.papi.cc@gmail.com>
2018-07-05 15:25:10 +0200

    Inicializing environment.

---
+++ src/main/java/com/example/planoramarsp/PlanoramarspApplication.java
@@ -0,0 +1,12 @@
+package com.example.planoramarsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoramarspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoramarspApplication.class, args);
+	}
+}
